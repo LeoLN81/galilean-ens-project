97b8202efab63ddbb83081e1dce42da0d0c2e9b4
LeoLN81 <118738585+LeoLN81@users.noreply.github.com>
2023-01-09 01:35:00 +0100

    Add files via upload

--- Simulator/methods.py
+++ Simulator/methods.py
@@ -2,6 +2,8 @@ import random
 from random import choices
 import networkx as nx
 import numpy as np
+from scipy.stats import lognorm
+#import graphdata
 
 #generate a directed graph (without parallel edges) with n nodes and a probability p that there is an edge between 2 nodes
 def random_Bernoulli_graph_gen(n,p) : 
@@ -111,6 +113,60 @@ def random_1_from_distrib_incremental(n, distrib):
             
             # Check if the edge is valid (i.e., not self-loop and no duplicate edges)
             if j<u and (i == 0 or not G.has_edge(u, j)):
+                # Add the edge to the graph
+                G.add_edge(u, j)
+                
+                # Increment the counter
+                i += 1
+    
+    # Return the generated graph
+    return G
+
+
+def random_1_from_lognormal(n, mean, std, scale):
+    """Generate a directed graph with a given number of nodes and a distribution of out-degrees.
+    
+    Parameters
+    ----------
+    n : int
+        The number of nodes in the graph.
+    mean : float
+        The mean of the log normal distribution.
+    std : float
+        The standard deviation of the log normal distribution.
+    scale : float
+        The scale of the log normal distribution.
+        
+    Returns
+    -------
+    networkx.DiGraph
+        The generated directed graph.
+    """
+    # Set up the log normal distribution using the mean and std
+    distribution = lognorm(mean, std, scale)
+    
+    # Initialize a directed graph
+    G = nx.DiGraph()
+    
+    # Add the nodes to the graph
+    G.add_nodes_from(range(n))
+    
+    # Loop through each node
+    for u in range(n):
+        # Generate a random number of out-degrees from the distribution
+        nb = int(distribution.rvs()/5)
+        print(nb)
+        
+        # Initialize a counter for the number of out-degrees
+        i = 0
+        
+        # Loop until the desired number of out-degrees is reached
+        while i != nb:
+            # Choose a random node to connect to
+            j = random.randint(0, n - 1)
+            
+            # Check if the edge is valid (i.e., not self-loop and no duplicate edges)
+            if j != u and (i == 0 or not G.has_edge(u, j)):
                 # Add the edge to the graph
                 G.add_edge(u, j)
                 
